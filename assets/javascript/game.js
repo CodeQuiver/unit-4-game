62f1c888325a30d28c9fdd916f1ddc2cf5f2f553
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-05-21 22:14:01 -0400

    more pseudocode and finished setting up random number generation functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,6 @@ var userScore = 0;
 var jewel1Value = 0;
 var jewel2Value = 0;
 var jewel3Value = 0;
-var jewel4Value = 0;
 
 
 //FUNCTIONS
@@ -22,26 +21,27 @@ var jewel4Value = 0;
 // computer selects random number for answer also assigns jewel values - this function will be called whenever a round ends to start the next round
 //userScore is also reset at beginning of new round
 function newRound() {
-    answer = Math.floor(Math.random() * 50);
+    // to get a random integer between 19 and 120 
+    // Math.floor(Math.random() * (max - min + 1)) + min;
+    answer = Math.floor(Math.random() * 102) + 19;
     console.log("computer answer is " + answer);
 
-    jewel1Value = Math.floor(Math.random() * 10);
+    jewel1Value = Math.floor(Math.random() * 12);
     console.log("Jewel 1 is " + jewel1Value);
 
-    jewel2Value = Math.floor(Math.random() * 10);
+    jewel2Value = Math.floor(Math.random() * 12);
     console.log("Jewel 2 is " + jewel2Value);
 
-    jewel3Value = Math.floor(Math.random() * 10);
+    jewel3Value = Math.floor(Math.random() * 12);
     console.log("Jewel 3 is " + jewel3Value);
 
-    jewel4Value = Math.floor(Math.random() * 10);
-    console.log("Jewel 4 is " + jewel4Value);
+    // jquery - update the display to show new computer score and hide both #winMessage and #lossMessage in html until ready to show them in course of round
 }
 
 // Win or Lose Function
-// if player score = answer then add 1 to wins and call newRound function
-// if player code > answer then add 1 to losses and call newRound function
-// if player score < answer nothing happens (game continues) so no code needed for that case
+// if player score === answer then add 1 to wins, update #scoreboard by showing "You Won!" #winMessage (hiding #lossMessage) and new win total, and call newRound function
+// if player score > answer then add 1 to losses, update #scoreboard by showing "You Lost!" #lossMessage (hiding #winMessage) and new loss total, and call newRound function
+// if player score < answer nothing happens (game continues and this is managed by jQuery code in main body) so no code needed for that case
 function winLoss(userScore, answer) {
     
 }
@@ -55,7 +55,7 @@ function winLoss(userScore, answer) {
 //call the function when page first loads to assign initial answer value and hidden jewel values
 newRound();
 
-/** For each jewel button: on click
+/** For each jewel button: on click (this is jQuery section for page interactions)
  * 1- add jewel value to userScore
  * 2- update display of userScore in scoreboard
  * 3- check if lost or won- call Win or Lose function
